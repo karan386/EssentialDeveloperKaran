eb7a9be254609418fe4d25fc22332c00bf032c79
Karan Sawant <karansawant7.ks@gmail.com>
2025-08-31 09:25:50 +0530

    Name change from 'RemoteFeedLoaderTest' to 'LoadFeedFromRemoteUseCaseTests'

--- EssentialFeed/EssentialFeedTests/Feed Api/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/LoadFeedFromRemoteUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-final class RemoteFeedLoaderTest: XCTestCase {
+final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
