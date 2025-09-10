c53e7227b0098624c2f4af611994b93510cb6e5c
Karan Sawant <karansawant7.ks@gmail.com>
2025-09-10 11:36:39 +0530

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -137,8 +137,10 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     // MARK: Helpers
-    func makeSUT() -> CodableFeedStore {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
     
 }
