79b62f1b1bee0acb7f2ff2e00d945dd77d5b7f12
Karan Sawant <karansawant7.ks@gmail.com>
2025-09-14 16:13:59 +0530

    Extract ManagedCache fetch request logic into a helper method within the managed model class

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -44,6 +44,12 @@ private class ManagedCache: NSManagedObject {
     var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
+    
+    static func find(context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
 }
 
 @objc(ManagedFeedImage)
@@ -104,9 +110,7 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
-                request.returnsObjectsAsFaults = false
-                if let cache = try context.fetch(request).first {
+                if let cache = try ManagedCache.find(context: context) {
                     completion(.found(
                         cache.localFeed,
                         cache.timestamp))
