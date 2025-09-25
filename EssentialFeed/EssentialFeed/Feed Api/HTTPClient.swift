78264b2539d7ce7e611492762e1c227d6cce4a7b
Karan Sawant <karansawant7.ks@gmail.com>
2025-09-25 09:39:07 +0530

    Replace custom HTTPClientResult enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeed/EssentialFeed/Feed Api/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed Api/HTTPClient.swift
@@ -7,13 +7,10 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
 /// The completion handler can be invoked in any thread.
 /// Clients are responsible to dispatch to appropriate threads, if needed.
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+    func get(from url: URL, completion: @escaping (Result) -> Void)
 }
